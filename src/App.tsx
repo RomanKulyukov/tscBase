37b1f09bdcf0cc39bcd1d3d8906f60fcb74caf5c
RomanKulyukov <zapryagli@gmail.com>
2023-05-04 21:59:36 +0300

    dsd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { Button, DatePicker, Space, version, Pagination, Input } from "antd";
 import "./App.css";
 import "antd/dist/reset.css";
-function App() {}
+function App() {
+  function handleSubmit (e:string) :any =>{
+    e.preventDefault();
+
+
+  }
+  return (
+    <div className="App">
+      <Input type="primary" ref={node =>(inputNode = node)} />
+      <Pagination></Pagination>
+    </div>
+  );
+}
 
 export default App;
