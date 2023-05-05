95e84da7dd5cdc5b127107d67178c3a771ba64e0
Roman Kulyukov <MY_NAME@example.com>
2023-05-05 23:11:54 +0300

    l

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { useState } from "react";
-import { Button, DatePicker, Space, version, Pagination, Input } from "antd";
+import { Button, Pagination, Input, Col, Row } from "antd";
 import "./App.css";
 import "antd/dist/reset.css";
 import Search from "./components/search/search";
-import buttonSearch from "./components/buttonSearch/buttonSearch";
+import ButtonSearch from "./components/buttonSearch/buttonSearch";
 function App() {
+  function rowFiller(n: number): any {
+    let a = [];
+    for (let i = 0; i < n; i++) {
+      a.push(
+        <Col span={24} key={i}>
+          result {i + 1}
+        </Col>
+      );
+    }
+    return a;
+  }
   return (
     <div className="App">
       <h1>Github search</h1>
       <Search></Search>
-      <ButtonSearch></ButtonSearch>
+      <ButtonSearch />
+      <Row>{rowFiller(30)}</Row>
     </div>
   );
 }
