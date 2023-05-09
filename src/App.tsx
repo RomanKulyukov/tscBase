67b7860a1b37f4e1b8cb5aa124dca43971a2b70f
Roman Kulyukov <zapryagli@gmail.com>
2023-05-09 17:24:23 +0300

    dsdsd

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { useState } from "react";
-import { Button, Pagination, Input, Col, Row } from "antd";
+import { Layout, Space, Col, Row, Button } from "antd";
 import "./App.css";
 import "antd/dist/reset.css";
 import Search from "./components/search/search";
 import ButtonSearch from "./components/buttonSearch/buttonSearch";
+import { createCallChain } from "typescript";
+
+const { Header, Footer, Sider, Content } = Layout;
+const CENTER = {
+  display: "flex",
+  justifyContent: "center",
+  marginBottom: "50px",
+};
+function filterBy(arg: string) {
+  console.log("filter by " + arg);
+}
 function App() {
   function rowFiller(n: number): any {
     let a = [];
     for (let i = 0; i < n; i++) {
       a.push(
-        <Col span={24} key={i}>
-          result {i + 1}
-        </Col>
+        <Layout style={{ width: "500px", marginBottom: "50px" }} key={i}>
+          <Header>Repository Name</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+        // <Col span={24} key={i}>
+        //   result {i + 1}
+        // </Col>
       );
     }
     return a;
@@ -20,9 +36,17 @@ function App() {
   return (
     <div className="App">
       <h1>Github search</h1>
-      <Search></Search>
-      <ButtonSearch />
-      <Row>{rowFiller(30)}</Row>
+      <Row style={CENTER}>
+        <Search></Search>
+        <ButtonSearch />
+      </Row>
+      <Row style={CENTER}>
+        <h5>Filter by:</h5>
+        <Button onClick={() => filterBy("stars")}>Stars</Button>
+        <Button onClick={() => filterBy("popularity")}>Popularity</Button>
+        <Button onClick={() => filterBy("something")}>Smth</Button>
+      </Row>
+      {rowFiller(30)}
     </div>
   );
 }
