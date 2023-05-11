6a905d393ca1f3ff14f2d7ae4e26f904fff85a30
Roman Kulyukov <MY_NAME@example.com>
2023-05-11 23:29:00 +0300

    d

--- src/components/searchPage/SearchPage.tsx
+++ src/components/searchPage/SearchPage.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./SearchPage.css";
 import InputBar from "../inputBar/inputBar";
 import FilterBar from "../filterBar/filterBar";
+
+import { Pagination } from "antd";
+
 function SearchPage() {
   return (
     <div className={"search"}>
@@ -11,18 +14,64 @@ function SearchPage() {
         <FilterBar></FilterBar>
       </div>
       <div>
-        <div>search result </div>
-        <div>search result </div>
-        <div>search result </div>
-        <div>search result </div>
-        <div>search result </div>
-        <div>search result </div>
-        <div>search result </div>
-        <div>search result </div>
-        <div>search result </div>
-        <div>search result </div>
+        <div>
+          <h3>result name</h3>
+          <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia,
+            incidunt, fugit earum minima a quae error fugiat quisquam nam
+            corporis facilis dolorum excepturi omnis doloremque quasi! Dolorum
+            cumque fuga ullam!
+          </div>
+          <div>
+            <div>result: watches</div>
+            <div>result: stars</div>
+            <div>result: forks</div>
+          </div>
+        </div>
+        <div>
+          <h3>result name</h3>
+          <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia,
+            incidunt, fugit earum minima a quae error fugiat quisquam nam
+            corporis facilis dolorum excepturi omnis doloremque quasi! Dolorum
+            cumque fuga ullam!
+          </div>
+          <div>
+            <div>result: watches</div>
+            <div>result: stars</div>
+            <div>result: forks</div>
+          </div>
+        </div>
+        <div>
+          <h3>result name</h3>
+          <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia,
+            incidunt, fugit earum minima a quae error fugiat quisquam nam
+            corporis facilis dolorum excepturi omnis doloremque quasi! Dolorum
+            cumque fuga ullam!
+          </div>
+          <div>
+            <div>result: watches</div>
+            <div>result: stars</div>
+            <div>result: forks</div>
+          </div>
+        </div>
+        <div>
+          <h3>result name</h3>
+          <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia,
+            incidunt, fugit earum minima a quae error fugiat quisquam nam
+            corporis facilis dolorum excepturi omnis doloremque quasi! Dolorum
+            cumque fuga ullam!
+          </div>
+          <div>
+            <div>result: watches</div>
+            <div>result: stars</div>
+            <div>result: forks</div>
+          </div>
+        </div>
       </div>
-      <div>pagination</div>
+      <Pagination defaultCurrent={1} total={50} />
     </div>
   );
 }
