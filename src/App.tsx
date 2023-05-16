7550c14ce8da7dd04f6f1011be15e5319167b4d2
Roman Kulyukov <MY_NAME@example.com>
2023-05-16 10:35:22 +0300

    n

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./App.css";
-import SearchPage from "./components/searchPage/SearchPage";
-
-function App() {
-  return <SearchPage></SearchPage>;
-}
-
-export default App;
