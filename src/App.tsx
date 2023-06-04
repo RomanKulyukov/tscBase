4c06f73a547c6b495f00360b2fb8b053d1c4183b
Roman Kulyukov <MY_NAME@example.com>
2023-06-04 19:38:50 +0300

    sd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { VFC } from "react";
 import "./App.css";
-import SearchPage from "./components/SearchPage/SearchPage";
 
 export const App: VFC = () => {
-  return <SearchPage />;
+  return <div>App</div>;
 };
